b1c5aae7c2c34a223aa85406e7a4651894f5c94c
JongHwan <whdghks845@gmail.com>
2026-05-22 15:23:22 +0900

    setting: 기본환경설정

---
+++ src/main/java/com/oopsw/kostaerpserver/KostaErpServerApplication.java
@@ -0,0 +1,13 @@
+package com.oopsw.kostaerpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KostaErpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KostaErpServerApplication.class, args);
+    }
+
+}
