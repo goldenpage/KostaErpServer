0cc1f3f17138dfab3b77501d63b736d09b8e2fac
차영훈 <younghun2562@naver.com>
2026-05-29 14:50:47 +0900

    refact: 식자재조회 테스트 어노테이션 추가

--- src/test/java/com/oopsw/kostaerpserver/service/FoodMaterialServiceTest.java
+++ src/test/java/com/oopsw/kostaerpserver/service/FoodMaterialServiceTest.java
@@ -1,15 +1,18 @@
 package com.oopsw.kostaerpserver.service;
 
 import com.oopsw.kostaerpserver.vo.FoodMaterial;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+@Slf4j
+@ActiveProfiles("test")
 @SpringBootTest
 public class FoodMaterialServiceTest {
 
