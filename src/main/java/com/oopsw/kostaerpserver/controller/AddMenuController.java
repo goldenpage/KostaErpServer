c7aca7697b093e9b9c36ad39a7abdaff329c1a6b
ajfal <gmibook1234@gmail.com>
2026-06-06 15:26:53 +0900

    feature/addMenu Thymeleaf적용 및 js, controller작성

--- src/main/java/com/oopsw/kostaerpserver/controller/AddMenuController.java
+++ src/main/java/com/oopsw/kostaerpserver/controller/AddMenuController.java
@@ -1,4 +1,57 @@
 package com.oopsw.kostaerpserver.controller;
 
+import com.oopsw.kostaerpserver.service.Interface.AddMenuService;
+import com.oopsw.kostaerpserver.vo.AddMenu;
+import com.oopsw.kostaerpserver.vo.MenuCategory;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
 public class AddMenuController {
+    private final AddMenuService addMenuService;
+
+    @GetMapping("/menu/add")
+    public String addMenu(Model model, HttpSession session) {
+        List<MenuCategory> categoryList = addMenuService.getMenuCategoryList("0000000000");
+        model.addAttribute("categoryList", categoryList);
+
+        return "addMenu";
+    }
+
+    @PostMapping("/menu/add")
+    public String addMenu(
+            @RequestParam(value = "menuName") List<String> menuName,
+            @RequestParam(value = "menuPrice") List<String> menuPrice,
+            @RequestParam(value = "menuCategoryId") List<String> menuCategoryId,
+            HttpSession session,
+            RedirectAttributes redirectAttributes){
+
+        try{
+            List<AddMenu> list = new ArrayList<>();
+            for(int i = 0; i < menuName.size(); i++){
+                AddMenu vo = new AddMenu();
+                vo.setMenuName(menuName.get(i));
+                vo.setMenuName(menuPrice.get(i));
+                vo.setMenuName(menuCategoryId.get(i));
+                list.add(vo);
+            }
+            for(AddMenu vo : list){
+                addMenuService.addMenu(vo);
+            }
+            redirectAttributes.addFlashAttribute("success", "메뉴 등록 성공");
+        }catch (Exception e){
+            redirectAttributes.addFlashAttribute("errorMessage", "메뉴 등록 실패");
+        }
+        return "redirect:/menu/add";
+    }
 }
--- src/main/java/com/oopsw/kostaerpserver/controller/AddMenuRestController.java
+++ src/main/java/com/oopsw/kostaerpserver/controller/AddMenuRestController.java
@@ -1,5 +1,70 @@
 package com.oopsw.kostaerpserver.controller;
 
+import com.oopsw.kostaerpserver.service.Interface.AddMenuService;
+import com.oopsw.kostaerpserver.vo.FoodMaterial;
+import com.oopsw.kostaerpserver.vo.MenuCategory;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class AddMenuRestController {
+    private final AddMenuService addMenuService;
+
+    @PostMapping("/menu/menucategory/add")
+    public Map<String, Object> addMenuCategory(@RequestBody Map<String, String> body) {
+        String menuCategory = body.get("menuCategory");
+        Map<String, Object> response = new HashMap<>();
+        try{
+            int result = addMenuService.checkMenuCategoryExists(menuCategory);
+            if(result > 0){
+                response.put("result", "fail");
+                response.put("message", "이미 존재하는 카테고리");
+                return response;
+            }
+            MenuCategory vo = new MenuCategory();
+            vo.setMenuCategory(menuCategory);
+            addMenuService.addMenuCategory(vo);
+
+            String categoryId = addMenuService.getCategoryId(menuCategory);
+
+            response.put("result", "success");
+            response.put("menuCategoryId", categoryId);
+            response.put("menuCategory", menuCategory);
+        } catch (Exception e) {
+            response.put("result", "fail");
+            response.put("message", "카테고리 추가 실패");
+        }
+        return response;
+    }
+
+    @DeleteMapping("/menu/menucategory/delete")
+    public Map<String, Object> deleteMenuCategory(@RequestBody Map<String, String> body) {
+        String menuCategory = body.get("menuCategory");
+        Map<String, Object> response = new HashMap<>();
+
+        try{
+            int exist = addMenuService.deleteMenuCategory(menuCategory);
+            if(exist > 0){
+                response.put("result", "fail");
+                response.put("message", "사용중인 카테고리");
+                 return response;
+            }
+
+            int result = addMenuService.deleteMenuCategory(menuCategory);
+            response.put("result", "success");
+            response.put("menuCategoryId", menuCategory + "가 삭제되었습니다.");
+        }catch (Exception e){
+            response.put("result", "fail");
+            response.put("message", "카테고리 삭제 오류");
+        }
+        return response;
+    }
 
 }
