92c69d708a907d08524fd6cf75459d60d1e9ca5e
ajfal <gmibook1234@gmail.com>
2026-06-09 11:19:11 +0900

    bId 받는 형식 변경 및 Controller url 임시 수정

--- src/main/java/com/oopsw/kostaerpserver/controller/AddFoodMaterialController.java
+++ src/main/java/com/oopsw/kostaerpserver/controller/AddFoodMaterialController.java
@@ -1,23 +1,12 @@
 package com.oopsw.kostaerpserver.controller;
 
-import com.oopsw.kostaerpserver.dto.addfoodmaterial.AddFoodMaterialRequest;
-import com.oopsw.kostaerpserver.dto.addfoodmaterial.AddFoodMaterialResponse;
 import com.oopsw.kostaerpserver.dto.addfoodmaterial.GetFoodCategoryListResponse;
 import com.oopsw.kostaerpserver.service.Interface.AddFoodMaterialService;
-import com.oopsw.kostaerpserver.vo.AddFoodMaterial;
 import com.oopsw.kostaerpserver.vo.FoodCategory;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.sql.Date;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -25,8 +14,8 @@ import java.util.List;
 public class AddFoodMaterialController {
     private final AddFoodMaterialService addFoodMaterialService;
 
-    @GetMapping("/foodmaterial/add")
-    public String addFoodMaterial(Model model, HttpSession session) {
+    @GetMapping("/foodmaterialadd")
+    public String addFoodMaterial(Model model) {
 
         List<FoodCategory> categoryList = addFoodMaterialService.getFoodCategoryList();
         model.addAttribute("categoryList", GetFoodCategoryListResponse.fromList(categoryList));
