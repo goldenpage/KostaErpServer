494b5e7f7e640d252d6db7f157810b247cae9b72
jong845 <jongtest845@gmail.com>
2026-06-14 21:25:38 +0900

    fix: entity namme chnage

--- src/main/java/com/oopsw/kostaerpserver/repository/entity/admin/AdminUser.java
+++ src/main/java/com/oopsw/kostaerpserver/repository/entity/admin/AdminUser.java
@@ -1,10 +1,12 @@
 package com.oopsw.kostaerpserver.repository.entity.admin;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,22 +16,30 @@ import lombok.Setter;
 
 @Builder
 @Entity
+@Table(name = "admin_user")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class AdminUser {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id")
     private int adminId;
+
+    @Column(name = "admin_name", nullable = false, unique = true)
     private String adminName;
+
+    @Column(nullable = false)
     private String pw;
+
+    @Column(nullable = false)
     private String role;
+
+    @Column(name = "created_at", nullable = false)
     private LocalDate createdAt;
-
-
-
 
 
 }
--- src/main/java/com/oopsw/kostaerpserver/repository/entity/expdate/ExpNotice.java
+++ src/main/java/com/oopsw/kostaerpserver/repository/entity/expdate/ExpNotice.java
@@ -25,23 +25,24 @@ import org.hibernate.annotations.UpdateTimestamp;
 public class ExpNotice {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "exp_notice_id")
     private int expNoticeId;
 
-    @Column(name = "bId", unique = true, nullable = false, length = 10)
+    @Column(name = "b_Id", unique = true, nullable = false, length = 10)
     private String bId;
 
-    @Column(name = "expAlert", nullable = false)
+    @Column(name = "exp_Alert", nullable = false)
     private boolean expAlert;
 
-    @Column(name = "expDays", nullable = false)
+    @Column(name = "exp_Days", nullable = false)
     private int expDays;
 
     @CreationTimestamp
-    @Column(name = "createdAt", updatable = false, nullable = false)
+    @Column(name = "created_At", updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
-    @Column(name = "updatedAt", nullable = false)
+    @Column(name = "updated_At", nullable = false)
     private LocalDateTime updatedAt;
 
     public void update(boolean expAlert, int expDays) {
