2e81d40b8df2216015512099e615ddad511ec9e5
gitdong95 <choidongseok95@gmail.com>
2026-07-04 22:56:32 +0900

    CookieUtil RT Cookie 설정용 Class 생성

---
+++ src/main/java/com/oopsw/kostaerpserver/auth/support/CookieUtil.java
@@ -0,0 +1,30 @@
+package com.oopsw.kostaerpserver.auth.support;
+
+import org.springframework.http.ResponseCookie;
+
+import java.time.Duration;
+
+public class CookieUtil {
+    //RT 쿠키정보 설정
+    public static ResponseCookie refreshCookie(String token, long maxAgeMillis) {
+        return ResponseCookie.from("refreshToken", token)
+                .httpOnly(true)
+                .secure(false)
+                .path("/api/auth/reissue")
+                .maxAge(Duration.ofMillis(maxAgeMillis))
+                .sameSite("Lax")
+                .build();
+    }
+
+    //RT 쿠키정보 삭제 설정
+    public static ResponseCookie deleteRefreshCookie() {
+        return ResponseCookie.from("refreshToken", "")
+                .httpOnly(true)
+                .secure(false)
+                .path("/api/auth/reissue")
+                .maxAge(0)
+                .sameSite("Lax")
+                .build();
+    }
+
+}
