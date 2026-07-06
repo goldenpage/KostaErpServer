7489a5ff6a37205f4075466e30fcad4be4d9e633
jong845 <jongtest845@gmail.com>
2026-07-06 17:56:32 +0900

    feat: userinfo 변경

--- src/main/java/com/oopsw/kostaerpserver/auth/config/SecurityConfig.java
+++ src/main/java/com/oopsw/kostaerpserver/auth/config/SecurityConfig.java
@@ -129,7 +129,6 @@ public class SecurityConfig {
                             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                             response.setContentType(MediaType.APPLICATION_JSON_VALUE);
                             response.getWriter().write("{\"message\":\"authentication required\"}");
-                            System.out.println(authException);
                         }));
 
         return http.build();
