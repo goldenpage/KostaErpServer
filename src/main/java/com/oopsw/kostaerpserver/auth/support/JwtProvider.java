88d38c8c7a3e0974c3ae75cd80922950145e5be5
gitdong95 <choidongseok95@gmail.com>
2026-07-08 16:15:48 +0900

    토큰 발급 컴포넌트 jjwt 에서 java-jwt로 변경

--- src/main/java/com/oopsw/kostaerpserver/auth/support/JwtProvider.java
+++ src/main/java/com/oopsw/kostaerpserver/auth/support/JwtProvider.java
@@ -1,22 +1,23 @@
 package com.oopsw.kostaerpserver.auth.support;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.Date;
 
+@Slf4j
 @Component
 public class JwtProvider {
     public static final String HEADER = "Authorization";
     public static final String PREFIX = "Bearer ";
 
-    private final SecretKey key;
+    //private final SecretKey key;
+    private final Algorithm algorithm;
     private final long accessExpMillis;
     private final long refreshExpMillis;
 
@@ -24,42 +25,63 @@ public class JwtProvider {
             @Value("${jwt.secret}") String secret,
             @Value("${jwt.access-expiration}")Duration accessExp,
             @Value("${jwt.refresh-expiration}")Duration refreshExp) {
-        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        //this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.algorithm = Algorithm.HMAC256(secret.getBytes(StandardCharsets.UTF_8));
         this.accessExpMillis = accessExp.toMillis();
         this.refreshExpMillis = refreshExp.toMillis();
     }
 
     public String createAccessToken(String username, String role) {
         long now = System.currentTimeMillis();
-        return Jwts.builder()
+        /*return Jwts.builder()
                 .subject(username)
                 .claim("role", role)
                 .issuedAt(new Date(now))
                 .expiration(new Date(now + accessExpMillis))
                 .signWith(key)
-                .compact();
+                .compact();*/
+        log.info("[JwtProvider] createAccessToken : username={}, role={}", username, role);
+        return JWT.create()
+                .withSubject(username)
+                .withClaim("role", role)
+                .withIssuedAt(new Date(now))
+                .withExpiresAt(new Date(now + accessExpMillis))
+                .sign(algorithm);
     }
 
     public String createRefreshToken(String username) {
         long now = System.currentTimeMillis();
-        return Jwts.builder()
+       /* return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date(now))
                 .expiration(new Date(now + refreshExpMillis))
                 .signWith(key)
-                .compact();
+                .compact();*/
+        log.info("[JwtProvider] createRefreshToken : username={}", username);
+        return JWT.create()
+                .withSubject(username)
+                .withIssuedAt(new Date(now))
+                .withExpiresAt(new Date(now + refreshExpMillis))
+                .sign(algorithm);
     }
 
-    public Claims parseClaims(String token) {
+    /*public Claims parseClaims(String token) {
         return Jwts.parser()
                 .verifyWith(key)
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
+    }*/
+    public DecodedJWT verify(String token) {
+        log.info("[JwtProvider] verify : {} 토큰검증 시작", token);
+        return JWT.require(algorithm)
+                .build()
+                .verify(token);
     }
 
     public String getUsername(String token) {
-        return parseClaims(token).getSubject();
+        /*return parseClaims(token).getSubject();*/
+        return verify(token).getSubject();
     }
 
     public long getRefreshExpMillis() {
